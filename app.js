d685cd293452b8552d923e998ee448b5ff3ecc07
DannyHeyes <63042730+DannyHeyes@users.noreply.github.com>
2021-03-01 17:01:27 +0100

    testing updates
    JS validation added, invalid email indicators added. removed validation from browser.

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-
 // changing background color of button on mouse down.
 
 const enter = document.querySelector('button');
 const error = document.querySelector('#errorIcon');
 const errorMessage = document.querySelector('.errorMessage');
+const input = document.querySelector('#emailInput');
 
 enter.addEventListener('click', () => {
-  enter.style.background = 'pink';
-  error.style.visibility = 'visible';
-  errorMessage.style.visibility = 'visible';
+
+
+  function validateEmail(input) {
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input == validRegex) {
+
+
+      return true;
+
+    } else {
+
+      enter.style.background = 'pink';
+      error.style.visibility = 'visible';
+      errorMessage.style.visibility = 'visible';
+
+      return false;
+
+    }
+
+  }
+
+  validateEmail();
 });
+
+
+
+
+// validation format : ^\S+@\S+$
